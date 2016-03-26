016d149befdfc618ef5ce232ed1e448413d47b9d
Thomas <hi@thomaspaulmann.com>
2016-03-26 15:18:03 +0100

    Simplify layout + scroll down animation

--- javascripts/scroll.js
+++ javascripts/scroll.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
+  var parallaxMultiplier = 0.67;
+  var fontOffset = 2;
+  
   $('.arrow').click(function(){
+    var fadeOutOffsetTop = $('#fade_out').offset().top;
 
-    var topOffset = 0.67 * $('.content_container').offset().top;
+    var top = parallaxMultiplier * fadeOutOffsetTop - fontOffset;
 
-    $('html,body').animate({
-       scrollTop: topOffset
-    });
-
-    });
+    $('html, body').animate({scrollTop: top});
+  });
 });
